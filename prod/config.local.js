4da99cb7ad71fe580f7d9adab2e2807a668a88ba
h3kom <h3kom@users.noreply.github.com>
2026-05-13 09:47:14 +0000

    deploy: fece4071a1fc57163fe803497d88853eefba5904

---
+++ prod/config.local.js
@@ -0,0 +1,4 @@
+window._GR_CONFIG = {
+  SUPABASE_URL: 'https://civkerrcyqgsqqjpccqe.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpdmtlcnJjeXFnc3FxanBjY3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMzkxNzAsImV4cCI6MjA5MzkxNTE3MH0.Q01QUWnwYm-aDAhbwi-Rb_kBU28s8Rx27J0RUkILY1U'
+};
