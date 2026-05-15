95c18dfd91dc81916494b3266f99d861756edadd
SAVUCU Hekim <hekim.savucu@8advisory.com>
2026-05-15 23:02:41 +0200

    chore: set vite base path to /grundriss-tool/dev/ for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/grundriss-tool/',
+  base: '/grundriss-tool/dev/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
